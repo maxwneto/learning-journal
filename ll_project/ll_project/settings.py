545c83c65f9fd4a9163654b8f92def04210801e3
maxwneto <maxwneto@gmail.com>
2024-08-03 06:49:38 -0300

    Add implementation of account app

--- ll_project/ll_project/settings.py
+++ ll_project/ll_project/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     #MyApps
-    'learning_logs'
+    'learning_logs',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# My settings.
+LOGIN_REDIRECT_URL = 'learning_logs:index'
